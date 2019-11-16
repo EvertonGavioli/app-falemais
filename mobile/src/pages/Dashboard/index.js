a4bd5ef4d7a9fa8b6ef9ea5b86bfb334be9d004a
Everton <vertopge@hotmail.com>
2019-11-16 17:21:40 -0300

    feat: dashboard page and styles

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, ScrollView} from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Title,
+  FieldContainer,
+  StyledPicker,
+  StyledInput,
+  TitleWithPlan,
+  PriceWithPlan,
+  TitleWithoutPlan,
+  PriceWithoutPlan,
+} from './styles';
 
 export default function Dashboard() {
   return (
-    <View>
-      <Text>DashBoard</Text>
-    </View>
+    <Container>
+      <ScrollView>
+        <Title>Simule os benefícios do plano FaleMais</Title>
+
+        <FieldContainer>
+          <Text>Origem:</Text>
+          <StyledPicker mode="dropdown">
+            <StyledPicker.Item label="Java" value="java" />
+            <StyledPicker.Item label="JavaScript" value="js" />
+          </StyledPicker>
+        </FieldContainer>
+
+        <FieldContainer>
+          <Text>Destino:</Text>
+          <StyledPicker mode="dropdown">
+            <StyledPicker.Item label="Java" value="java" />
+            <StyledPicker.Item label="JavaScript" value="js" />
+          </StyledPicker>
+        </FieldContainer>
+
+        <FieldContainer>
+          <Text>Minutos:</Text>
+          <StyledInput keyboardType="numeric" />
+        </FieldContainer>
+
+        <FieldContainer>
+          <Text>Plano FaleMais:</Text>
+          <StyledPicker mode="dropdown">
+            <StyledPicker.Item label="Java" value="java" />
+            <StyledPicker.Item label="JavaScript" value="js" />
+          </StyledPicker>
+        </FieldContainer>
+
+        <TitleWithPlan>Com FaleMais</TitleWithPlan>
+        <PriceWithPlan>R$ 10.000,00</PriceWithPlan>
+
+        <TitleWithoutPlan>Sem FaleMais</TitleWithoutPlan>
+        <PriceWithoutPlan>R$ 20.000,00</PriceWithoutPlan>
+      </ScrollView>
+    </Container>
   );
 }
--- mobile/src/pages/Dashboard/styles.js
+++ mobile/src/pages/Dashboard/styles.js
@@ -1,3 +1,72 @@
 import styled from 'styled-components/native';
+import color from '../../styles/colors';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  margin: 10px;
+  border-radius: 4px;
+  background: #fff;
+`;
+
+export const Title = styled.Text`
+  text-align: center;
+  margin: 20px;
+  font-size: 22px;
+  font-weight: bold;
+  color: ${color.primary};
+`;
+
+export const FieldContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  height: 50px;
+  margin: 4px 20px;
+  padding: 0px 10px;
+  border: 1px solid ${color.secondary};
+  border-radius: 8px;
+`;
+
+export const StyledPicker = styled.Picker`
+  flex: 1;
+  margin-left: 10px;
+`;
+
+export const StyledInput = styled.TextInput`
+  flex: 1;
+  margin-left: 10px;
+  margin-bottom: 5px;
+  border-bottom-width: 1;
+  border-bottom-color: ${color.secondary};
+  font-size: 16px;
+`;
+
+export const TitleWithPlan = styled.Text`
+  margin: 10px 20px 0px;
+  font-size: 22px;
+  font-weight: bold;
+  color: #228b22;
+`;
+
+export const PriceWithPlan = styled.Text`
+  margin-left: 40px;
+  font-size: 32px;
+  font-weight: bold;
+  color: ${color.primary};
+`;
+
+export const TitleWithoutPlan = styled.Text`
+  text-align: right;
+  margin: 10px 20px 0px;
+  font-size: 22px;
+  font-weight: bold;
+  color: #dc143c;
+`;
+
+export const PriceWithoutPlan = styled.Text`
+  text-align: right;
+  margin-right: 40px;
+  font-size: 32px;
+  font-weight: bold;
+  color: ${color.primary};
+`;
